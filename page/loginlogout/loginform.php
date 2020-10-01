ca5f1794e334c990af8f6482f27b568659077c80
Virtus Tan <virtustanofficial@gmail.com>
2020-10-01 16:44:38 +0700

    Master

--- page/loginlogout/loginform.php
+++ page/loginlogout/loginform.php
@@ -35,15 +35,17 @@
       <div class="row">
         <form class="col s12">
           <div class="row">
-            <div class="input-field col s6">
+            <div class="input-field col s12">
               <i class="material-icons prefix">account_circle</i>
-              <input id="icon_prefix" type="text" class="validate">
-              <label for="icon_prefix">First Name</label>
+              <input id="email" type="text" class="validate">
+              <label for="email">E-mail</label>
             </div>
-            <div class="input-field col s6">
-              <i class="material-icons prefix">phone</i>
-              <input id="icon_telephone" type="tel" class="validate">
-              <label for="icon_telephone">Telephone</label>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">fiber_pin</i>
+              <input id="icon_prefix" type="password" class="validate">
+              <label for="icon_prefix">Password</label>
             </div>
           </div>
         </form>
