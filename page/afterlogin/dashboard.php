d392e29a3164349a1fdbc84b571f6bd843c1190c
Virtus Tan <virtustanofficial@gmail.com>
2020-10-05 21:14:32 +0700

    Master

--- page/afterlogin/dashboard.php
+++ page/afterlogin/dashboard.php
@@ -12,7 +12,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="http://www.pngall.com/wp-content/uploads/2016/09/Musical-Notes-Free-Download-PNG.png" type="image/png" sizes="16x16">
-    s
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../../layout/dash.css">
     <script type="text/javascript" src="../../script/dashnav.js"></script>
