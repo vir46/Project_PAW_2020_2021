c73cdfaf575191de558bb2114f660076837170e8
albertfarizt <albertsijabat10@gmail.com>
2020-10-05 15:46:12 +0700

    2nd master

---
+++ page/afterlogin/dashboard.php
@@ -0,0 +1,184 @@
+<?php
+session_start();
+if (!$_SESSION['isLogin']) {
+header("location: ../loginPage.php");
+}else {
+include('../db.php');
+}
+echo '
+            //masih salah
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="http://www.pngall.com/wp-content/uploads/2016/09/Musical-Notes-Free-Download-PNG.png" type="image/png" sizes="16x16">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link rel="stylesheet" href="layout/dashboard.css">
+    <meta name="description" content="Find Your Music Production, Music Need, anything about music here!">
+    <title>MIS - Welcome</title>
+</head>
+<body>
+    <!-- Navigasi Bar Dashboard-->
+    <div class="navbar-fixed">
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#!" class="brand-logo"><img class="logo-br" src="http://www.pngall.com/wp-content/uploads/2016/09/Musical-Notes-Free-Download-PNG.png" 
+                style="height: 30pt; margin: 10pt; margin-left:20pt ;"></a>
+            <ul class="right hide-on-med-and-down">
+                <li class="list-nav"><a class="link-nav" href="page/product/logfalse.php">Product</a></li>                
+                <li class="list-nav"><a class="link-nav" href="page/contact/contactmain.php">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <!-- HEAD BANNER -->
+      <div class="headtitle">
+        <div class="texthead">
+            <h1 class="title">Your Daily Music Need</h1>
+            <div class="browsebtn"><a class="browsebtn" href="">BROWSE</a></div>
+        </div>
+        <div class="videobg-cont">
+            <video class="videobg-cont" oncontextmenu="return false;" autoplay loop muted>
+                <source class="video" src="video/background.mp4" type="video/mp4">
+            </video>
+        </div>
+      </div>
+      <!-- Floating Image -->
+      <div class="imgtitle">
+        <a href="" class="imglink">
+            <img class="imglink" src="https://megamusic.blob.core.windows.net/images/0022601_taylor-k24ce-koa-acoustic-guitar-with-pickup-and-cutaway.jpg">
+            <div class="textfloat">
+                Premium<br>
+                Music Instrument
+            </div>
+        </a>
+        <a href="" class="imglink">
+            <img class="imglink" src="https://images.squarespace-cdn.com/content/v1/523763a4e4b05227e95792ab/1569595419692-ZWJTEUQ9NHJMNT8O2A36/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/DSC_0275.jpg">
+            <div class="textfloat">
+                Cheap<br>
+                Studio Rent
+            </div>
+        </a>
+        <a href="" class="imglink">
+            <img class="imglink" src="https://www.thebroadcastbridge.com/cache/uploads/content_images/Mic_care_789_526_70_s.jpg">
+            <div class="textfloat">
+                High Quality<br>
+                Studio Record
+            </div>
+        </a>
+      </div>
+      <!-- Promo Bar -->
+        <!-- HEADTITLE -->
+      <div class="promoct">
+        <h2 class="titlepromo">New on Us!</h2>
+      </div>
+        <!-- Banner Promo -->
+      <div class="promoct">
+          <a class="promolink" href="">
+              <div class="txtpromo">
+                  Children Course
+              </div>
+              <img class="imgpromo" src="https://www.musikalessons.com/blog/wp-content/uploads/2017/09/shutterstock_446935882.jpg">
+            </a>
+          <a class="promolink" href="">
+              <div class="txtpromo">
+                  Public Course
+              </div>
+              <img class="imgpromo" src="https://images.squarespace-cdn.com/content/v1/5804d617414fb5f8c4214140/1575031482406-VH6PZ3R3SX13P38IPZ18/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/Daniel+Korea+Recital%28Deagu%29-5.jpg">
+          </a>
+      </div>
+      <!-- Review Banner -->
+      <div class="reviewtitle containlogo">
+            <img class="reviewlogo" src="https://www.bluecorona.com/wp-content/uploads/2015/05/26-googleplusreviews.jpg">
+        </div>
+        <div class="reviewtitle">
+            <!-- Card 1 -->
+            <div class="row">
+                <div class="col s12 m7">
+                  <div class="card">
+                    <div class="card-image">
+                      <img class="card-image" src="https://asset-a.grid.id/crop/0x77:1052x733/x/photo/2020/03/28/2013533003.jpg">
+                      <span class="card-title" style="color: black; font-weight: bold;
+                      -webkit-text-stroke: 0.6px white;">Singer</span>
+                    </div>
+                    <div class="card-content">
+                      <p>Cukup dua kata ajasih kalau dari aku, "The Best" . Buat yang ga pernah
+                          denger toko ini, parah sih.
+                      </p>
+                    </div>
+                    <div class="card-action">
+                      <a href="https://www.instagram.com/ari_lasso/">Ari Lasso</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Card2 -->
+              <div class="row">
+                <div class="col s12 m7">
+                  <div class="card">
+                    <div class="card-image">
+                      <img class="card-image" src="https://media.suara.com/pictures/970x544/2020/07/10/94473-eka-gustiwana-instagramatekagustiwana.jpg">
+                      <span class="card-title" style="color: black; font-weight: bold;
+                      -webkit-text-stroke: 0.6px white;">Music Composer</span>
+                    </div>
+                    <div class="card-content">
+                      <p>Semua barang ada disini, mulai dari peralatan preproduksi sampai alat mix lengkap.</p>
+                    </div>
+                    <div class="card-action">
+                      <a href="https://www.instagram.com/ekagustiwana/">Eka Gustiwana</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Card 3 -->
+              <div class="row">
+                <div class="col s12 m7">
+                  <div class="card">
+                    <div class="card-image">
+                      <img class="card-image" src="https://asset.indosport.com/article/image/273985/misellia_ikwan_jess_no_limit-169.jpg">
+                      <span class="card-title" style="color: black; font-weight: bold;
+                      -webkit-text-stroke: 0.6px white;">Content Creator</span>
+                    </div>
+                    <div class="card-content">
+                      <p>Seneng bangeeeeettt! akhirnya bisa bikin musik disini + rekaman disini,
+                          hasilnya ga aku kira, makasih MIS.
+                      </p>
+                    </div>
+                    <div class="card-action">
+                      <a href="https://www.instagram.com/misellia_/">Misellia Ikwan</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+      </div>
+      <!-- Footer -->
+      <footer class="page-footer" style="margin-top: 15vh; width: 100vw;">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Music Instrumental Store</h5>
+              <p class="grey-text text-lighten-4">MIS is the biggest music store in Indonesia, cooperating with many international music producers.</p>
+            </div>
+            <div class="col l4 offset-l1 t12">
+              <h5 class="white-text textcollab">In Collaboration with</h5>
+              <ul>
+                <li><a href="https://hq.vevo.com/"><img class="imgfooter" src="https://s3-us-west-1.amazonaws.com/static-spin-com/files/2018/05/vevo_logo_white-1527177207-compressed.png"></a></li>
+                <li><a href="https://music.disney.com/"><img class="imgfooter" src="https://charlottehelenbaconfoundation.org/wp-content/uploads/2014/06/gallery-3-disney-music-logo-XL.jpg"></a></li>
+                <li><a href="https://www.sonymusic.com/"><img class="imgfooter" src="https://miro.medium.com/max/750/1*RT5Vwgtkd-OX679qdLn6vw.png  "></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+          © 2020 Kelompok 7 PAW | Atma Jaya Yogyakarta University
+          </div>
+        </div>
+      </footer>
+</body>
+</html>
+    '
+?>
+    
